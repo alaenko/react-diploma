684c9fb496caf18d24921add03e2c24e4a25b802
Alena Laenko <a.laenko@netology-group.ru>
2019-10-07 10:42:07 +0300

    add /

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const urls = {
   topSales: process.env.REACT_APP_BACKEND_HOST + '/api/top-sales',
   categories: process.env.REACT_APP_BACKEND_HOST + '/api/categories',
   items: process.env.REACT_APP_BACKEND_HOST + '/api/items',
-	order: process.env.REACT_APP_BACKEND_HOST + 'api/order',
+	order: process.env.REACT_APP_BACKEND_HOST + '/api/order',
 }
 
 export default urls;
